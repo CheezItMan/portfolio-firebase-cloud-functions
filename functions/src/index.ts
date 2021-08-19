96e9d126d9fef112fa62d139b4118e9cc4dbeaae
Chris McAnally <chris@adadevelopersacademy.org>
2021-08-18 20:40:47 -0700

    initial functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,4 @@
-import * as functions from "firebase-functions";
+import * as functions from 'firebase-functions';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -7,3 +7,33 @@ import * as functions from "firebase-functions";
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+import * as admin from 'firebase-admin';
+
+admin.initializeApp();
+
+
+export const addMessage = functions.https.onRequest(async (req, res) => {
+// Grab the text parameter.
+  const original = req.query.text;
+  // Push the new message into Firestore using the Firebase Admin SDK.
+  const writeResult = await admin.firestore().collection('messages')
+      .add({original: original});
+    // Send back a message that we've successfully written the message
+  res.json({result: `Message with ID: ${writeResult.id} added.`});
+});
+
+// Listens for new messages added to /messages/:documentId/original 
+//    and creates an
+// uppercase version of the message to /messages/:documentId/uppercase
+export const makeUppercase = functions.firestore
+    .document('/messages/{documentId}')
+    .onCreate((snapshot, context) => {      
+      const original = snapshot.data().original;
+
+      functions.logger.log('Uppercasing', context.params.documentId, original);
+
+      const uppercase = original.toUpperCase();
+
+      return snapshot.ref.set({uppercase}, {merge: true});
+    });
