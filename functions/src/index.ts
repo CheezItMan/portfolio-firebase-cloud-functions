0f592234c06211090106856efeb03abaaadfd0ab
Chris McAnally <chris@adadevelopersacademy.org>
2021-09-06 19:25:13 -0700

    minimize functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,5 +4,27 @@ import * as admin from 'firebase-admin';
 admin.initializeApp();
 
 export const addMessage = functions.https.onRequest(async (req, res) => {
-  console.log('testing');
+  if (req.method == 'POST') {
+    console.log('Got a post request!');
+    console.log(req.body);
+
+    const {name, from, subject, message} = req.body;
+    console.log(req.body);
+    if ( name && from && subject && message) {
+      admin.firestore().collection('mail').add({
+        to: 'mcanallyc@gmail.com',
+        from: from,
+        message: {
+          subject: subject,
+          html: message,
+        },
+      });
+      const apiData = await admin.firestore().collection('config')
+          .doc('APIs').get();
+
+      console.log('*****APIData*****');
+      console.log(apiData);
+      console.log('*****APIData*****');
+    }
+  }
 });
