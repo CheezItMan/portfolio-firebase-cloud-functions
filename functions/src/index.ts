9bda69e317c2b1cabcc104d1f73bdd49d30658dc
Chris McAnally <chris@adadevelopersacademy.org>
2021-09-11 18:22:57 -0700

    cleanup message

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,30 +1,88 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
-
+import * as sgMail from '@sendgrid/mail';
 admin.initializeApp();
+
+type ConfigData = {
+  SENDGRID_API_KEY: string,
+  destination: string,
+};
+
+type EmailRequestBody = {
+  name: string, 
+  from: string, 
+  subject: string,
+  message: string,
+}
 
 export const addMessage = functions.https.onRequest(async (req, res) => {
   if (req.method == 'POST') {
-    console.log('Got a post request!');
-    console.log(req.body);
-
-    const {name, from, subject, message} = req.body;
-    console.log(req.body);
-    if ( name && from && subject && message) {
-      admin.firestore().collection('mail').add({
-        to: 'mcanallyc@gmail.com',
-        from: from,
-        message: {
-          subject: subject,
-          html: message,
-        },
-      });
-      const apiData = await admin.firestore().collection('config')
+    const {name, from, subject, message} = req.body as EmailRequestBody;
+    console.log(req.body);    
+      const snapshot = await admin.firestore().collection('config')
           .doc('APIs').get();
+      if (snapshot.exists) {
+        const configData = snapshot.data() as ConfigData;
+        if ( name && from && subject && message) {
+          admin.firestore().collection('mail').add({
+            to: configData.destination,
+            from: from,
+            message: {
+              subject: subject,
+              html: message,
+            },
+          });
 
-      console.log('*****APIData*****');
-      console.log(apiData);
-      console.log('*****APIData*****');
+        const fullTextHTMLMessage = `<section>
+          <h2>Portfolio Email from: ${from},</h2>
+          <article>
+            <h3>Subject:  ${subject}</h3>
+            <br />
+            ${message}
+          </article>
+        </section>
+        `;
+
+        const fullTextMessage = `
+        From: ${from},
+        Subject: ${subject}
+        Message: ${message}
+        `
+
+        const msg = {
+          to: configData.destination,
+          from: configData.destination,
+          subject,
+          text: fullTextMessage,
+          html: fullTextHTMLMessage,
+        };
+        try {
+          sgMail.setApiKey(configData.SENDGRID_API_KEY);
+          await sgMail.send(msg);
+        } catch (error) {
+          res.statusCode = 500;
+          res.send({
+            ok: false,
+            message: 'Error Sending Email',
+          });
+          console.log('Error sending email');
+          console.log(error);
+          return;
+        }
+        res.statusCode = 201;
+        res.send({
+          'ok': true,
+          'message': 'Email Sent!',
+        });
+        console.log('Email Sent Successfully');
+      } else {
+        res.statusCode = 500;
+        res.send({
+          ok: false,
+          message: 'Cannot connect to get mail configuration data',
+        });
+        console.log('Cannot get email');
+      }
     }
   }
 });
