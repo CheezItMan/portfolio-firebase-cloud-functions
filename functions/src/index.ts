dae359100e53ea174fa02bf37eb339229f2be473
Chris McAnally <chris@adadevelopersacademy.org>
2021-09-12 20:09:03 -0700

    revise sending email

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,14 +1,21 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
-import * as sgMail from '@sendgrid/mail';
 import * as corsModule from 'cors';
-const cors = corsModule(({origin: true}));
-admin.initializeApp();
 
-type ConfigData = {
-  SENDGRID_API_KEY: string,
-  destination: string,
-};
+const cors = corsModule({
+  origin: true,
+});
+admin.initializeApp();
+// const db = admin.firestore();
+
+// Sendgrid config
+import * as sgMail from '@sendgrid/mail';
+const API_KEY = functions.config().sendgrid.key;
+// const TEMPLATE_ID = functions.config().sendgrid.template;
+sgMail.setApiKey(API_KEY);
+const EMAIL_RECIPIENT = functions.config().send.recipient;
+const FROM_EMAIL = functions.config().send.from;
+
 
 type EmailRequestBody = {
   name: string,
@@ -17,80 +24,40 @@ type EmailRequestBody = {
   message: string,
 }
 
-export const addMessage = functions.https.onRequest(async (req, res) => {
-  if (req.method == 'POST') {
-    const {name, from, subject, message} = req.body as EmailRequestBody;
-    const snapshot = await admin.firestore().collection('config')
-        .doc('APIs').get();
+export const addMessage = functions.https.onRequest((req, res) => {
+  return cors(req, res, async () => {
+    const {name, subject, message} = req.body as EmailRequestBody;
+    console.log('Sending Email');
+    console.log(`API_KEY = ${API_KEY}`);
+    console.log(`EMAIL_RECIPIENT = ${EMAIL_RECIPIENT}`);
 
-    if (snapshot.exists) {
-      const configData = snapshot.data() as ConfigData;
-      if ( name && from && subject && message) {
-        admin.firestore().collection('mail').add({
-          to: configData.destination,
-          from: from,
-          message: {
-            subject: subject,
-            html: message,
-          },
-        });
+    const text = `<div>
+      <h2>Email from Portfolio</h2>
+      <h3>FROM:  ${name}</h3>
+      <h3>Subject: ${subject}</h3>
+      <p>${message}</p>
+    `;
 
-        const fullTextHTMLMessage = `<section>
-          <h2>Portfolio Email from: ${from},</h2>
-          <article>
-            <h3>Subject:  ${subject}</h3>
-            <br />
-            ${message}
-          </article>
-        </section>
-        `;
-
-        const fullTextMessage = `
-        From: ${from},
-        Subject: ${subject}
-        Message: ${message}
-        `;
-
-        const msg = {
-          to: configData.destination,
-          from: configData.destination,
-          subject,
-          text: fullTextMessage,
-          html: fullTextHTMLMessage,
-        };
-        try {
-          sgMail.setApiKey(configData.SENDGRID_API_KEY);
-          await sgMail.send(msg);
-        } catch (error) {
-          cors(req, res, () => {
-            res.set('Access-Control-Allow-Headers', 'Content-Type');
-            res.statusCode = 500;
-            res.send({
-              ok: false,
-              message: 'Error Sending Email',
-            });
-            console.log('Error sending email');
-            console.log(error);
-          });
-          return;
-        }
-        cors(req, res, () => {
-          res.set('Access-Control-Allow-Headers', 'Content-Type');
-          res.statusCode = 201;
-          res.send({
-            'ok': true,
-            'message': 'Email Sent!',
-          });
-          console.log('Email Sent Successfully');
-        });
-      } else {
-        res.statusCode = 500;
-        res.send({
-          ok: false,
-          message: 'Cannot connect to get mail configuration data',
-        });
-        console.log('Cannot get email');
-      }
+    const msg = {
+      to: EMAIL_RECIPIENT,
+      from: FROM_EMAIL,
+      subject: `${name} sent a message`,
+      text: text,
+      html: text,
+    };
+    try {
+      await sgMail.send(msg);
+      res.status(201).send({
+        ok: true,
+        message: 'Email Sent!',
+      });
+    } catch (error) {
+      console.log(`sgmail error = ${error}`);
+      res.status(500).send({
+        ok: false,
+        message: 'Email sending error',
+      });
+      return;
     }
-  }
+  });
 });
